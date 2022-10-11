a2f9788c7a4446e67cd1fd1831f30719ee926c10
Allecan <allecan52@gmail.com>
2022-10-11 09:10:08 -0600

    Initial structure of the home page
    Maquetado preliminar para la pagina raiz / home

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import emojiLizzard from "./assets/imgs/emoji-lizzard.png";
+import emojiStopwatch from "./assets/imgs/emoji-stopwatch.png";
+import emojiUnicorn from "./assets/imgs/emoji-unicorn.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col mx-[156px]">
+      <div className="flex flex-col items-center gap-2 mt-10">
+        <p>Level Up - Pool Oct 2022</p>
+        <h1>Proyetos de Kelvin Cano</h1>
       </div>
-      <h1 className=''>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="flex flex-row gap-6 items-center mx-auto">
+        <div className="card flex flex-col px-[100px] py-[68px] items-center justify-center gap-6">
+          <img src={emojiStopwatch} alt="emoji-stopwatch" />
+          <h2>Counter</h2>
+        </div>
+        <div className="card flex flex-col px-[100px] py-[68px] items-center justify-center gap-6">
+          <img src={emojiLizzard} alt="emoji-lizzard" />
+          <h2>Camaleon</h2>
+        </div>
+        <div className="card flex flex-col px-[100px] py-[68px] items-center justify-center gap-6">
+          <img src={emojiUnicorn} alt="emoji-unicorn" />
+          <h2>Pokedex</h2>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
