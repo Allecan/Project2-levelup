b167ed037679402051326c1f9bb4dcdbfa4526fd
Allecan <allecan52@gmail.com>
2022-10-11 19:48:45 -0600

    Project 2 finished
    Ya cambia el color segun si el contador es 0, negativo o positivo

--- src/pages/counter.jsx
+++ src/pages/counter.jsx
@@ -3,27 +3,47 @@ import "../css/counter.css";
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [color, setColor] = useState("");
+
+  function verifyCounter(count) {
+    if (count < 0) {
+      setColor("negative");
+    } else if (count > 0) {
+      setColor("positive");
+    } else {
+      setColor("neutro");
+    }
+  }
   return (
     <div className="flex flex-col mx-[156px] gap-[96px] min-h-screen">
-      <div className="flex flex-col items-center gap-4 mt-[20%]">
+      <div className="flex flex-col items-center gap-4 mt-[16%]">
         <h2>Counter</h2>
-        <h1 className="text-[119.66px]">{count}</h1>
+        <h1 className={`text-[119.66px] ${color}`}>{count}</h1>
       </div>
       <div className="flex flex-row gap-[72px] justify-center items-center">
         <button
-          onClick={() => setCount((count) => count - 1)}
+          onClick={() => {
+            setCount(count - 1);
+            verifyCounter(count - 1);
+          }}
           className="w-[200px] h-[72px]"
         >
           <h2>Decrease</h2>
         </button>
         <button
-          onClick={() => setCount(0)}
+          onClick={() => {
+            setCount(0);
+            verifyCounter(0);
+          }}
           className="w-[144px] h-[72px] reset-button"
         >
           <h2>Reset</h2>
         </button>
         <button
-          onClick={() => setCount((count) => count + 1)}
+          onClick={() => {
+            setCount(count + 1);
+            verifyCounter(count + 1);
+          }}
           className="w-[200px] h-[72px]"
         >
           <h2>Increase</h2>
